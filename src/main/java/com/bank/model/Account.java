0b55395a74ed45aa1d0662b2e7283a805c97540c
mng2214 <mng2214@gmail.com>
2024-04-24 20:47:42 -0500

    validation added for Account

--- src/main/java/com/bank/model/Account.java
+++ src/main/java/com/bank/model/Account.java
@@ -2,6 +2,9 @@ package com.bank.model;
 
 import com.bank.enums.AccountStatus;
 import com.bank.enums.AccountType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -15,10 +18,13 @@ import java.util.UUID;
 public class Account {
 
     private UUID id;
+    @NotNull
+    @Positive
     private BigDecimal balance;
+    @NotNull
     private AccountType accountType;
-  //  @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date creationDate;
+    @NotNull
     private Long userId;
     private AccountStatus accountStatus;
 
