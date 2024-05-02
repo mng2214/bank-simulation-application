bfe0020f95583d9c4113d7fa90765263ea6e350f
mng2214 <mng2214@gmail.com>
2024-05-01 19:40:05 -0500

    validation added for Account

--- src/main/java/com/bank/model/Transaction.java
+++ src/main/java/com/bank/model/Transaction.java
@@ -1,5 +1,9 @@
 package com.bank.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -11,10 +15,15 @@ import java.util.UUID;
 @Data
 @Builder
 public class Transaction {
-
+    @NotNull
     private UUID sender;
+    @NotNull
     private UUID receiver;
+    @Positive
+    @NotNull
     private BigDecimal amount;
+    @Size(min = 2, max = 255)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$\"")
     private String message;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createDate;
