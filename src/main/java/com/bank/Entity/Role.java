4ffa84f6a97d5ee5e10d65b38606aa67166c554d
mng2214 <mng2214@gmail.com>
2024-06-26 18:40:11 -0500

    security added

---
+++ src/main/java/com/bank/Entity/Role.java
@@ -0,0 +1,20 @@
+package com.bank.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+}
---
+++ src/main/java/com/bank/Entity/User.java
@@ -0,0 +1,32 @@
+package com.bank.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @OneToOne
+    private Role role;
+
+    private boolean isEnabled;
+
+
+
+}
